f4b663f7269c796682de2c961be61f18d5d77e34
AlvaroRaul123 <rh904930@gmail.com>
2021-10-24 13:39:39 -0500

    actualizacion

---
+++ database/factories/AdministrativoFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Administrativo;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AdministrativoFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Administrativo::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ database/migrations/2021_10_24_181645_create_tutors_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTutorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tutors', function (Blueprint $table) {
+            $table->id();
+            $table->integer('rol');
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->integer('numero');
+            $table->string('tutor_auxiliar');
+            $table->string('domicilio');
+            $table->string('usuario')->unique();
+            $table->string('contraseña');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tutors');
+    }
+}
